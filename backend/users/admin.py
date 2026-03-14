a25742028813a943ad1cbce1e1c7f65c45e21f4e
steve-ongera <gadafisteve001@gmail.com>
2026-03-14 10:11:05 +0300

    made updates

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'is_email_verified', 'is_staff', 'date_joined')
+    list_filter = ('is_email_verified', 'is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email', 'phone')
+    readonly_fields = ('date_joined', 'last_login')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {
+            'fields': ('phone', 'avatar', 'date_of_birth', 'is_email_verified')
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {
+            'fields': ('phone', 'avatar', 'date_of_birth', 'is_email_verified')
+        }),
+    )
